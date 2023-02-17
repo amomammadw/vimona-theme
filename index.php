f9c53fde2944154636b90c2564dcb8702691ffda
amomammadw82 <67383118+amomammadw82@users.noreply.github.com>
2023-02-17 19:43:42 +0330

    init vimona wp theme

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(  ) ?>>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('title') ?></title>
+    <link rel="stylesheet" href="<?php get_stylesheet_uri(); ?>">
+    <?php wp_head() ?>
+</head>
+
+<body>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta suscipit iusto esse ad laudantium corrupti,
+        veritatis quo voluptatem quia adipisci accusantium ex perspiciatis et aliquam, inventore facere accusamus, quasi
+        error!
+    </div>
+    <?php wp_footer(  ) ?>
+</body>
+
+</html>
